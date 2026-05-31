f41cf0d7c8af39feb22b6fd53f0886ca3c91cba9
Sumit Kumar <tbeast935@gmail.com>
2026-06-01 00:58:14 +0530

    auth.js

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -99,7 +99,7 @@ export const login = async (req, res) => {
 
         secure: process.env.NODE_ENV === "production",
 
-        sameSite: "strict",
+        sameSite: "none",
 
         maxAge: 7 * 24 * 60 * 60 * 1000,
       });
