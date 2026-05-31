55c304439efd18f532ae8603bb7cc8c44986fc50
Sumit Kumar <tbeast935@gmail.com>
2026-06-01 00:43:38 +0530

    fb

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -7,7 +7,7 @@ import { serverUrl } from "./App";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain:  import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  authDomain: "bookmytickets-7577b.firebaseapp.com",
   projectId: "bookmytickets-7577b",
   storageBucket: "bookmytickets-7577b.firebasestorage.app",
   messagingSenderId: "219759870148",
