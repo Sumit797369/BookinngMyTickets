b27cd0996a4fa21ba8e3e362fe72fb346d66a66d
Sumit Kumar <tbeast935@gmail.com>
2026-06-01 00:24:17 +0530

    verc

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://bookinng-my-tickets.vercel.app/"],
+    origin: ["http://localhost:5173","https://bookinng-my-tickets.vercel.app"],
     credentials: true,
   }),
 );
